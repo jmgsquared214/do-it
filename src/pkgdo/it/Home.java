b9ca00f83047dcf85f5ff0ff19a68f132ee710ac
Jamie Michelle Greene <Jamie.M.Greene@aexp.com>
2017-01-10 13:11:58 -0700

    idea for UI DG

--- src/pkgdo/it/Home.java
+++ src/pkgdo/it/Home.java
@@ -1,11 +1,17 @@
 package pkgdo.it;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.CheckBox;
 import javafx.stage.Stage;
 import javafx.scene.*;
 
@@ -19,11 +25,23 @@ public class Home extends Application {
 		grid.setGridLinesVisible(false);
 		grid.setHgap(5);
 		grid.setVgap(20);
+		
+		TableView table = new TableView();
+		table.setMinHeight(300);
+		table.setMinWidth(550);
+		
+		TableColumn column1 = new TableColumn("Priority");
+		TableColumn column2 = new TableColumn("Description");
+		TableColumn column3 = new TableColumn("Progress");
+		table.getColumns().addAll(column1, column2, column3);
+		
+		GridPane.setConstraints(table, 1, 1, 3, 1);
 
 		Label tableArea = new Label("This is where tasks table will come");
 		tableArea.setMinWidth(600);
 		
 		GridPane.setConstraints(tableArea, 1, 1, 3, 1);
+		
 		
 		TextField taskName = new TextField();
 		taskName.setPromptText("Enter Task Name");
@@ -44,7 +62,17 @@ public class Home extends Application {
 		addButton.setMinWidth(100);
 		GridPane.setConstraints(cancelButton, 3, 3);
 		
-		grid.getChildren().addAll(tableArea, taskName, priority, addButton, cancelButton);
+		HBox progressArea = new HBox();
+		progressArea.getChildren().addAll(new Label("Progress"),
+										new Spinner<Integer>(0, 100, 0),
+										new CheckBox("Completed"));
+		progressArea.setAlignment(Pos.CENTER_RIGHT);
+		progressArea.setSpacing(10);
+		GridPane.setConstraints(progressArea, 1, 3, 2, 1);
+		
+		
+		
+		grid.getChildren().addAll(tableArea, table, taskName, priority, addButton, cancelButton, progressArea);
 		
 		Scene scene = new Scene(grid, 600, 400);
 		
